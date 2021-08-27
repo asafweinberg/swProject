2610ecb2fc1b4c496afcc8fb3ceada4d159e2753
asafweinberg <74104951+asafweinberg@users.noreply.github.com>
2021-08-27 14:13:56 +0300

    code organizations

--- spkmeans.h
+++ spkmeans.h
@@ -37,69 +37,97 @@ typedef struct{
 #define MaxJacobiIter 100
 #define epsilon 0.000000000000001
 
+/*-----------------------------------------------------------------------------*/
+/* ******************************************** SPK Functions ******************************************** */
+/*-----------------------------------------------------------------------------*/
 
-//void testPrint(char arr[]);
-double ** kmeansFunc(int k, int maxIter, int numOfPoints, int d, Node* pointsMatrix, cluster* centroids);
-Node* getPoints(char* fileName, int* numOfPoints, int* finald);
+/* -------------------------------------------- Run Flow Functions -------------------------------------------- */
+void runWam(Node* points, int numOfPoints, int d);
+void runDdG(Node* points, int numOfPoints, int d);
+matrix * runLnorm(Node* points, int numOfPoints, int d, int printMat0);
+matrix * runSpk(int k, Node* points, int numOfPoints, int d); //returns matrix T nxk
+void runJacobi(Node* points, int numOfPoints, int d, int k);
+double ** runMainFlow(int k, char* myGoal, char* fileName, int* finalK, int* numOfPoints); //return T or NULL
 
-void doKmeans(cluster* clusters, Node* points, int d, int k, int numOfPoints, int maxIteratiorns);
-int continueLoop( int convergence, int count, int maxIter);
-void iterateClusters(cluster* clusters, int length, int d);
-int minCluster(double* point, cluster* clusters, int K, int d);
-double calcDistance(double* p1, double* p2, int d);
-void addPoint(cluster c, double* point, int d);
-int checkCon(cluster* clusters, int k, int d);
-Node* addNext(Node* node, double* point);
-Node* addCurrentNext(Node* node, double* point);
-void printClusters(cluster* clusters, int k, int d);
-void freeMemo(cluster* clusters, Node* points, int k, int numOfpoints);
 
-cluster* makeClustersSp(int k, Node* points, int numOfPoints,int d);
-
+/* -------------------------------------------- goals Functions -------------------------------------------- */
 matrix* formMatW(Node* points, int numOfPoints, int d);
+matrix* formMatT(matrix * matU, int free1);
 matrix* formMatD(matrix* matW, int freeW);
 matrix* formMatLnorm(matrix* matD , matrix* matW , int freeD, int freeW);
-matrix* minusRootMat(matrix* mat, int free1);
-matrix* formMatI(int dimention);
-matrix* pointsToMat(Node* points, int numOfPoints, int d);
 
-matrix* mulMatrices(matrix* mat1, matrix* mat2, int free1, int free2);
-matrix* addMatrices(matrix* mat1, matrix* mat2, int dec, int free1, int free2);
-matrix* newMatrix(int rows, int columns);
-void freeMatrix(matrix * m);
 
-void jacobiAlg(matrix* mat, matrix** eigenValues, matrix** eigenVectors);
+/* -------------------------------------------- jacobi Functions ------------------------------------------- */
+void jacobiAlg(matrix* lNorm, matrix** eigenValues, matrix** eigenVectors);
 matrix* getRotationMatrixValues(matrix* mat, double *c, double *s, int *rowPivot, int *colPivot);
-void findLargestCell(matrix* mat, int *row, int *col) ;
-matrix* calcNextJacobiMatrix(matrix* matA, double c, double s, int i, int j);
+void findLargestCell(matrix* mat, int *row, int *col);
 matrix* setPivotMatrix(int row, int col, double c, double s, int dim);
-int hasConvergence(matrix* matA, matrix* matB);
+matrix* calcNextJacobiMatrix(matrix* matA, double c, double s, int i, int j);
+int hasJacobiConvergence(matrix* matA, matrix* matB);
 double calcOff(matrix* m);
 matrix* calcInitialVectorsFromJacobi(matrix* eigenValues, matrix* eigenVectors, int initialK);
 int* eigenGapHeuristic(matrix* matA, int* k);
 int findMaxGap(eigenVal* values, int length);
-int isDiagonal(matrix* m);
-matrix* copyMatrix(matrix* m);
-void printMatrix(matrix* A);
-
-
 int compareEigenVal(const void * a, const void * b);
-matrix * runLnorm(Node* points, int numOfPoints, int d, int printMat);
-double ** runMainFlow(int k, char* myGoal, char* fileName, int* finalK, int* numOfPoints); //return T or NULL
- 
 
+
+/* -------------------------------------------- matrices Functions ------------------------------------------- */
+matrix* minusRootMat(matrix* mat, int free1);
+matrix* mulMatrices(matrix* mat1, matrix* mat2, int free1, int free2);
+matrix* addMatrices(matrix* mat1, matrix* mat2, int dec, int free1, int free2);
+matrix* formMatI(int dimention);
+int isDiagonal(matrix* m);
+matrix* newMatrix(int rows, int columns);
+matrix* copyMatrix(matrix* m);
+
+
+/*---------------------------------------------------------------------------------------------------*/
+/* ******************************************** KMeans Functions ******************************************** */
+/*---------------------------------------------------------------------------------------------------*/
+
+/* -------------------------------------------- Data Convertions Functions ------------------------------------------- */
 Node* getPointsFromT(double ** TDoubleArr, int d, int numOfpoints);
 cluster * getClustersFromT(double ** TDoubleArr, int finalK);
+matrix* pointsToMat(Node* points, int numOfPoints, int d);
 double ** matToArr(matrix * m, int free1);
+
+
+/* -------------------------------------------- Kmeans Algorithm Functions ------------------------------------------- */
+Node* getPoints(char* fileName, int* numOfPoints, int* finald);
+double ** kmeansFunc(int k, int maxIter, int numOfPoints, int d, Node* pointsMatrix, cluster* centroids);
+void doKmeans(cluster* clusters, Node* points, int d, int k, int numOfPoints, int maxIteratiorns);
+
+
+/* -------------------------------------------- Kmeans Helpers Functions ------------------------------------------- */
+int getMinCluster(double* point, cluster* clusters, int K, int d);
+double calcDistance(double* p1 , double* p2 ,int d);
+void addPointToCluster(cluster c, double* point, int d);
+int checkKmeansConvergence(cluster* clusters, int k, int d);
+int continueLoop( int convergence, int count, int maxIter);
+void iterateClusters(cluster* clusters, int length, int d);
+
+
+/*---------------------------------------------------------------------------------------------------*/
+/* ******************************************** General Functions ******************************************** */
+/*---------------------------------------------------------------------------------------------------*/
+
+/* -------------------------------------------- List Functions ------------------------------------------- */
+Node* addNext(Node* node, double* point);
+Node* addCurrentNext(Node* node, double* point);
+
+
+/* -------------------------------------------- Free Memory Functions ------------------------------------------- */
+void freeMatrix(matrix * m);
+void free2DArray(double ** TDoubleArr, int d, int numOfpoints);
+void freeKmeansMemory(cluster* clusters, Node* points, int k, int numOfpoints);
+void freePointsList(Node * points, int numOfpoints);
+
+
+/* -------------------------------------------- Print Functions ------------------------------------------- */
+void printMatrix(matrix* A);
 void printEigenfromMat(matrix* m);
-
-
-
-
-
-
-
-
-
-void printEigenArr(eigenVal * arr, int length); 
+void printMatOutput(matrix *m); //TODO check if it's according to rules and replace with printMatrix
+void printEigenArr(eigenVal * arr, int length);
 void printNodesList(Node * points, int d);
+void printLst(Node* lst, int d); // TODO: duplicate method?
+void printClusters(cluster* clusters, int k, int d);
